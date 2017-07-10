a1cc00b7bc72177ffc20b3ce7553c9700e8b1e58
skatoch <angelic.sam@gmail.com>
2017-07-09 22:46:06 -0700

    Permutation of string

---
+++ Recursion/PermuteString.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void swap(string& str, int i, int j)
+{
+  char tmp = str[i];
+  str[i] = str[j];
+  str[j] = tmp;
+}
+
+int permuteString(string& str, int index)
+{
+  if (index == str.length()-1)
+    cout << str << endl;
+
+  for (int i = index; i < str.length(); ++i)
+  {
+    swap(str, index, i);
+    permuteString(str, index+1);
+    swap(str, index, i);
+  }
+}
+
+int permuteStringWrapper(string& str)
+{
+  return permuteString(str, 0);
+}
+
+int main(int argc, char** argv)
+{
+  string str = "aaa";
+  permuteStringWrapper(str);
+  return 0;
+}
+
